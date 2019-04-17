5db62ce2396606e330414a1ad2b750582453076a
Daniel de Kock <daniel@riot.network>
2019-04-17 15:22:14 +0200

    Update install.sh
    Edited so installation works for ARM systems ( Rpi )

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ cp ngrok.yml /opt/ngrok
 sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
 
 cd /opt/ngrok
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
 
 systemctl enable ngrok.service
 systemctl start ngrok.service
